a2c6f28d184f19c4f7c780b5e1b86330651bbe6d
Vishal Anand <karnvishal021@gmail.com>
2025-04-24 11:41:19 +0530

    Initial Commit

---
+++ ADBConnector/ADBConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADBConnectorApp.swift
+//  ADBConnector
+//
+//  Created by Vishal Anand on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ADBConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADBConnector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADBConnector
+//
+//  Created by Vishal Anand on 24/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
